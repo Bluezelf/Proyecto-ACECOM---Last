e8d644815c1adaf45309be82f2d19a186dfc2a90
Vite <omar.vite.a@uni.pe>
2023-11-27 16:30:37 -0500

    Avance grafico dinamico

--- FrontEnd/src/components/custom/AcChart.jsx
+++ FrontEnd/src/components/custom/AcChart.jsx
@@ -3,70 +3,144 @@ import * as echarts from "echarts";
 
 const AcChart = () => {
   useEffect(() => {
-    const month = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
+    const month = [
+      "Jan",
+      "Feb",
+      "Mar",
+      "Apr",
+      "May",
+      "Jun",
+      "Jul",
+      "Aug",
+      "Sep",
+      "Oct",
+      "Nov",
+      "Dec",
+    ];
     const value = [30, 50, 60, 80, 70, 50, 39, 60, 100, 50, 60, 80];
 
-    const data = month.map((month, index) => ({
+    const info = month.map((month, index) => ({
       name: month,
       value: value[index],
     }));
 
     var myChart = echarts.init(document.getElementById("chart"));
+
+    var app = {};
+
+    const categories = (function () {
+      let now = new Date();
+      let res = [];
+      let len = 10;
+      while (len--) {
+        res.unshift(now.toLocaleTimeString().replace(/^\D*/, ""));
+        now = new Date(+now - 2000);
+      }
+      return res;
+    })();
+    
+    const data = (function () {
+      let res = [];
+      let len = 10;
+      while (len--) {
+        res.push(Math.round(Math.random() * 1000));
+      }
+      return res;
+    })();
+
     myChart.setOption({
-      tooltip: {},
-      xAxis: {
-        data: data.map(item => item.name),
-        axisLabel: {
-          textStyle: {
-            
-            fontWeight:  'bold', 
-          }
-        }
-      },
-      yAxis: {
-        splitLine: {
-          show: false
+      tooltip: {
+        trigger: "axis",
+        axisPointer: {
+          type: "cross",
+          label: {
+            backgroundColor: "#283b56",
+          },
         },
-        axisLabel: {
-          textStyle: {
-            
-            fontWeight:  'bold',
-          }
-        }
       },
+      toolbox: {
+        show: true,
+        feature: {
+          saveAsImage: {},
+        },
+      },
+      dataZoom: {
+        show: false,
+        start: 0,
+        end: 100,
+      },
+      xAxis: [
+        {
+          type: "category",
+          boundaryGap: true,
+          data: categories,
+          axisLabel: {
+            textStyle: {
+              fontWeight: "bold",
+            },
+          },
+        },
+      ],
+      yAxis: [
+        {
+          type: "value",
+          scale: true,
+          max: 1200,
+          min: 0,
+          boundaryGap: [0.2, 0.2],
+          splitLine: {
+            show: false,
+          },
+          axisLabel: {
+            textStyle: {
+              fontWeight: "bold",
+            },
+          },
+        },
+      ],
       series: [
         {
           name: "Air quality",
           type: "bar",
           barWidth: 30,
-          data: data.map(item => item.value),
+          data: data,
           symbolSize: "7",
           lineStyle: {
             color: "#101D42",
-            width: 1, 
-          },
-          
-          itemStyle: {
-            color: function (params) {
-              var value = params.value;
-              if (value < 50) {
-                return "#42a548";
-              } else if (value < 100) {
-                return "#dfb213";
-              } else {
-                return "#af0505";
-              }
-            },
+            width: 1,
           },
         },
       ],
       grid: {
         width: "90%",
         left: "5%",
-        top: "10%",
+        top: "12%",
         bottom: "10%",
       },
     });
+
+    app.count = 11;
+
+    setInterval(function () {
+      let axisData = new Date().toLocaleTimeString().replace(/^\D*/, "");
+      data.shift();
+      data.push(Math.round(Math.random() * 1000));
+      categories.shift();
+      categories.push(axisData);
+   
+      myChart.setOption({
+        xAxis: [
+          {
+            data: categories,
+          },
+        ],
+        series: [
+          {
+            data: data,
+          },
+        ],
+      });
+    }, 2100);
 
     window.addEventListener("resize", () => {
       myChart.resize();
